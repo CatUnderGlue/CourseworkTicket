b5640bae122b7424fb260d9b071e39868b0bef3c
CatUnderGlue <warmonds1996@gmail.com>
2022-12-22 13:59:52 +0500

    Выполнил задачу из билета

---
+++ src/Human.java
@@ -0,0 +1,12 @@
+public class Human {
+    // 3 вопрос
+    private String fullname;
+    private int age;
+    private String city;
+
+    public Human(String fullname, int age, String city){
+        this.fullname = fullname;
+        this.age = age;
+        this.city = city;
+    }
+}
